ab78490d4ac2910c5104bbb23f3b56786f986300
shnvandiepen1 <s.vandiepen@student.maastrichtuniversity.nl>
2021-06-09 17:36:35 +0200

    Added Cell class

---
+++ Cell.java
@@ -0,0 +1,13 @@
+public class Cell {
+    
+    private int x;
+    private int y;
+    private boolean alive;    
+
+    public Cell(int x, int y){
+        this.x = x;
+        this.y = y;  
+        this.alive = false;
+    }
+
+}
